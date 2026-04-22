10217a2f1e077dc2148888e6d5c10ea00a1a4c6a
KokWenKai <kokwenkai773@gmail.com>
2026-04-22 16:43:30 +0800

    fix: add missing jacoco plugin and task to build script

---
+++ app/src/test/java/code/name/monkey/retromusic/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package code.name.monkey.retromusic
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    @Test
+    fun pipeline_isWorking() {
+        assertEquals(4, 2 + 2)
+    }
+}
